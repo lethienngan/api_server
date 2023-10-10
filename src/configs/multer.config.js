c88437c8a4a79bb57217e344cde311fabcc8ccc2
lethienngan <lethienngan95@gmail.com>
2023-10-10 09:35:40 +0700

    update multer

--- src/configs/multer.config.js
+++ src/configs/multer.config.js
@@ -1,16 +1,5 @@
 const multer = require("multer");
 const upload = multer({
-    // storage: multer.diskStorage({
-    //     destination: function (req, file, cb) {
-    //         cb(null, `${__dirname}`);
-    //     },
-    //     filename: function (req, file, cb) {
-    //         cb(
-    //             null,
-    //             file.fieldname + "_" + Date.now() + "_" + file.originalname
-    //         );
-    //     },
-    // }),
     storage: multer.memoryStorage(),
     limits: {
         fileSize: 10 * 1024 * 1024,
